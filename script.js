cb6e0af8b408b9268a5adba26d9f76f207f79987
Pedrocoutinho9 <pedrinhoregobarros@gmail.com>
2025-08-27 17:55:09 -0700

    Part B done

---
+++ script.js
@@ -0,0 +1,24 @@
+
+let name = "Pedro";          
+let age = 30;                
+let isStudent = true;        
+let address = null;          
+let phone;                   
+
+
+const country = "Canada";    
+const pi = 3.14159;          
+const isAdult = true;        
+const favoriteColor = null;  
+const hobby = undefined;     
+
+
+var city = "Vancouver";      
+var score = 100;             
+var isPassed = false;        
+var middleName = null;       
+var nickname;                
+
+console.log(name, age, isStudent, address, phone);
+console.log(country, pi, isAdult, favoriteColor, hobby);
+console.log(city, score, isPassed, middleName, nickname);
